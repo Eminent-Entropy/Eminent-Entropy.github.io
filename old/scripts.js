70ff54111fa69b0ec0ce5f5d09180c649abbe58c
Aiden G <eminent.entropy@gmail.com>
2022-10-20 00:36:17 -0500

    stuff

---
+++ old/scripts.js
@@ -0,0 +1,13 @@
+sidebarHide = function() {
+	let root = document.querySelector(':root');
+	let rootStyle = getComputedStyle(root);
+	let sidebarExpand = rootStyle.getPropertyValue('--side-bar-width-expand');
+	let sidebarHide = rootStyle.getPropertyValue('--side-bar-width-hide');
+
+	if (rootStyle.getPropertyValue('--side-bar-width') != sidebarExpand) {
+		root.style.setProperty('--side-bar-width', sidebarExpand)
+	}
+	else {
+		root.style.setProperty('--side-bar-width', sidebarHide)
+	}
+};
